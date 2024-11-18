2ec2319b3842f78207d0bd1d4a7481fa10c01808
Gregoire <gregoire.legros@edu.devinci.fr>
2024-11-18 13:02:15 +0100

    initialisation du projet

---
+++ Projet Boogle/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Projet_Boogle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionnaire mots = new Dictionnaire("fr");
+            string a = mots.toString();
+            Console.WriteLine(a);
+        }
+    }
+}
